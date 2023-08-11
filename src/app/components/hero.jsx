58f1266e8bd912ae53ef2577acdd8c90d6721d8f
Aldair Ramos <raldair7@gmail.com>
2023-08-11 10:31:16 -0600

    New Sections

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -4,6 +4,8 @@ import { Transition } from '@headlessui/react'
 
 import Cards from './team.jsx'
 import Para from './parallaxHero.jsx'
+import Skills from './skills.jsx'
+import Projects from './projects.jsx'
 
 export default function hero() {
   return (
@@ -11,6 +13,8 @@ export default function hero() {
       	
     	<Para />
 	<Cards />
+	<Skills />
+	<Projects />
     </div>
   );
 }
